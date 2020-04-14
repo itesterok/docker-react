d15d01e40709c9b12a673b8eab890dac3e2c738a
Andrii <itesterok@gmail.com>
2020-04-14 23:11:40 +0200

    Updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
-import ReactDOM from 'react-dom';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Blabla There!/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Blabla There!/i);
+  const linkElement = getByText(/Changes from the feature branch!/i);
   expect(linkElement).toBeInTheDocument();
 });
